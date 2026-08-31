15fe4c444fe1ceb8243f8aef5accbb46cae4e3d2
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-08-31 23:14:08 +0530

    using elif conditional stmt is compared the values

--- compare.py
+++ compare.py
@@ -1,9 +1,18 @@
 x=int(input("What's x ?"))
 y=int(input("What's y ?"))
 
+#using if condition
 if x>y:
     print("x is greater than y ")
 if x<y:
     print("x is less than y ")
 if x==y:
+    print("x equals to y ")
+
+#using the elif condition
+if x>y:
+    print("x is greater than y ")
+elif x<y:
+    print("x is less than y ")
+elif x==y:
     print("x equals to y ")
