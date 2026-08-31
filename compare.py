aa6a87833b45705e34c27322dd2b0d12ff1766e0
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-08-31 23:29:32 +0530

    using else conditional stmt compared the values

--- compare.py
+++ compare.py
@@ -16,3 +16,11 @@ elif x<y:
     print("x is less than y ")
 elif x==y:
     print("x equals to y ")
+    
+#using else condition 
+if x>y:
+    print("x is greater than y ")
+if x<y:
+    print("x is less than y ")
+else:
+    print("x equals to y ")
