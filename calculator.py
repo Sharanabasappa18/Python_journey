0a006bbd92c3831bc97194431e12478ba19eceb0
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-08-31 19:38:53 +0530

    using def done square of a number

--- calculator.py
+++ calculator.py
@@ -1,13 +1,19 @@
-x=float(input("enter x value :"))
-y=float(input("enter y value :"))
-# x=7
-# y=9
+# x=float(input("enter x value :"))
+# y=float(input("enter y value :"))
+# # x=7
+# # y=9
 
-# print(x/y)
-# print(int(input("enter x value :"))+int(input("enter y value :")))
-z=round(x/y)
-print(z)
+# # print(x/y)
+# # print(int(input("enter x value :"))+int(input("enter y value :")))
+# z=round(x/y)
+# print(z)
 
 
-
+def main():
+    x=int(input("what's x ?"))
+    print(f"square of {x} is ",square(x))
+    
+def square(s):
+    return pow(s,8)
+# main() 
 
