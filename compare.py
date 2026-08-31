8a46294d6e51fc5749b0c310d8335bf8b65cb1dc
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-08-31 23:42:03 +0530

    using 'or' logical operatos  compared the values

--- compare.py
+++ compare.py
@@ -2,25 +2,31 @@ x=int(input("What's x ?"))
 y=int(input("What's y ?"))
 
 #using if condition
-if x>y:
-    print("x is greater than y ")
-if x<y:
-    print("x is less than y ")
-if x==y:
-    print("x equals to y ")
+# if x>y:
+#     print("x is greater than y ")
+# if x<y:
+#     print("x is less than y ")
+# if x==y:
+#     print("x equals to y ")
 
 #using the elif condition
-if x>y:
-    print("x is greater than y ")
-elif x<y:
-    print("x is less than y ")
-elif x==y:
-    print("x equals to y ")
+# if x>y:
+#     print("x is greater than y ")
+# elif x<y:
+#     print("x is less than y ")
+# elif x==y:
+#     print("x equals to y ")
     
 #using else condition 
-if x>y:
-    print("x is greater than y ")
-if x<y:
-    print("x is less than y ")
+# if x>y:
+#     print("x is greater than y ")
+# if x<y:
+#     print("x is less than y ")
+# else:
+#     print("x equals to y ")
+    
+#using or condition 
+if x<y or x>y:
+    print("x is not equals to y ")
 else:
     print("x equals to y ")
