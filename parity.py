47fc5892eff84e564fa1792d8e97b9cbef687ed8
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-09-01 22:11:29 +0530

    using own function predicted even or odd

---
+++ parity.py
@@ -0,0 +1,22 @@
+# x=int(input("X value:"))
+
+# if x%2==0:
+#     print("Even")
+# else:
+#     print("Odd")
+    
+#using own function 
+def main():
+    x=int(input("X value:"))
+    if is_even(x):
+        print("Even")
+    else:
+        print("Odd")
+        
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False     
+    
+main()  
