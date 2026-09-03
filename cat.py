8bcee4468b4f6ea7a4a09883a3654bc0644ec694
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-09-03 22:24:17 +0530

    for loop

--- cat.py
+++ cat.py
@@ -10,9 +10,24 @@
     # i=i-1
 
 ## ##
-i=0
-while i<3:            # check while i<=3:
-    print("meow")
-    i += 1              #check  i=i+1 
+# i=0
+# while i<3:            # check while i<=3:
+#     print("meow")
+#     i += 1              #check  i=i+1 
 1
 
+# #for loop 
+# for i in [0,11,2]:
+#     print("meow")
+    
+# print("meow\n"*3,end="")
+
+def main():
+    meow(3)
+
+def meow(n):
+    for _ in range(n):
+        print("meow")
+main()
+        
+    
