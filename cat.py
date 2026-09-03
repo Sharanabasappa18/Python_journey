f1bd0cac57fd23e72da16d2e1807ec9719d09387
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-09-03 18:03:00 +0530

    while loop

---
+++ cat.py
@@ -0,0 +1,18 @@
+#### it will loop unlimited meow ####
+# i=3
+# while i!=0:
+#     print("meow")
+
+## it will print 3 meow ##
+# i=3
+# while i!=0:
+    # print("meow")
+    # i=i-1
+
+## ##
+i=0
+while i<3:            # check while i<=3:
+    print("meow")
+    i += 1              #check  i=i+1 
+1
+
