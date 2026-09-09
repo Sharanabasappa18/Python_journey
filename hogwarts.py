a275b337246da2f621415be38fac6e47ca043de7
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-09-09 23:13:00 +0530

    add print table using dict

--- hogwarts.py
+++ hogwarts.py
@@ -1,4 +1,4 @@
-students=["hermione","Ron","harry"]
+# students=["hermione","Ron","harry"]
 # print(students[0])
 # print(students[1])
 # print(students[2])
@@ -13,18 +13,29 @@ students=["hermione","Ron","harry"]
     
 
 #dict
-students={
-    "Hermione":"Gryffindor",
-    "Harry":"Gryffindor",
-    "Ron":"Gryffindor",
-    "Draco":"Slytherin",
-}
-print(students["Hermione"])
-print(students["Harry"])
-print(students["Ron"])
-print(students["Draco"])
+# students={
+    # "Hermione":"Gryffindor",
+    # "Harry":"Gryffindor",
+    # "Ron":"Gryffindor",
+    # "Draco":"Slytherin",
+# }
+# print(students["Hermione"])
+# print(students["Harry"])
+# print(students["Ron"])
+# print(students["Draco"])
+# for i in students:
+    # print(i,students[i],sep=",")
 
 
+
+students=[
+    {"name":"Hermione","house":"Gryffindor","patronus":"otter"},
+    {"name":"Harry","house":"Gryffindor","patronus":"Stag"},
+    {"name":"Ron","house":"Gryffindor","patronus":"Jack Russel terrier"},
+    {"name":"Draco","house":"Slytherin","patronus":"None"},
+    ]
+for student in students:
+    print(student["name"],student["house"],student["patronus"],sep=",")
 
 
 
