6acec30cada29413fdc89ec46d2060653bb87e65
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-09-10 22:32:33 +0530

    Add square pattern function

--- mario.py
+++ mario.py
@@ -7,8 +7,29 @@
 # main()
 
 
+# def main():
+#     print_row(3)
+# def print_row(width):
+#     print("?" * width)
+# main()
+
 def main():
-    print_row(3)
+    print_square(3)
+def print_square(size):
+    
+    #For each row in square      &  in place of size we can write number for only one loop 
+    for i in range(size):
+        
+        # #For each brick in row 
+        # for j in range(size):
+            
+        #     # Print brick 
+        #     print("#",end="")
+        # print()
+        # print("#"* size)        #in one line u can print the square without inner j loop 
+        print_row(size)
 def print_row(width):
-    print("?" * width)
+    print("#"* width )
 main()
+
+            
