d3887744a024a17b6ab920b441f7cf32f293043c
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-09-14 01:47:42 +0530

    Add pytest tests for square function

--- test_calculator_2.py
+++ test_calculator_2.py
@@ -1,37 +1,52 @@
-from calculator_2 import square
+# from calculator_2 import square
 
-def main():
-    test_square()
+# def main():
+#     test_square()
     
-def test_square():
-    # if square(2) != 4:
-    #     print(" 2 square was not  4")
-    # if square(3) != 9:
-    #     print(" 3 square was not  9") 
+# def test_square():
+#     # if square(2) != 4:
+#     #     print(" 2 square was not  4")
+#     # if square(3) != 9:
+#     #     print(" 3 square was not  9") 
         
-    # assert square(2) == 4
-    # assert square(3) == 9
+#     # assert square(2) == 4
+#     # assert square(3) == 9
     
-    try:
-        assert square(2) == 4
-    except AssertionError:
-        print(" 2 square was not  4")
-    try:
-        assert square(3) == 9
-    except AssertionError:
-       print(" 3 square was not  9")  
-    try:
-        assert square(-2) == 4
-    except AssertionError:
-        print(" -2 square was not  4")
-    try:
-        assert square(-3) == 9
-    except AssertionError:
-        print(" -3 square was not  9")  
-    try:
-        assert square(0) == 0
-    except AssertionError:
-           print(" 0 square was not  0")  
+#     try:
+#         assert square(2) == 4
+#     except AssertionError:
+#         print(" 2 square was not  4")
+#     try:
+#         assert square(3) == 9
+#     except AssertionError:
+#        print(" 3 square was not  9")  
+#     try:
+#         assert square(-2) == 4
+#     except AssertionError:
+#         print(" -2 square was not  4")
+#     try:
+#         assert square(-3) == 9
+#     except AssertionError:
+#         print(" -3 square was not  9")  
+#     try:
+#         assert square(0) == 0
+#     except AssertionError:
+#            print(" 0 square was not  0")  
     
-if __name__=="__main__":
-    main()
+# if __name__=="__main__":
+#     main()
+
+### pytest ###
+import pytest
+from calculator_2 import square
+def test_square():
+    assert square(2)==4
+    assert square(3)==9
+def test_negative():  
+    assert square(-2)==4
+    assert square(-3)==9
+def test_zero():
+    assert square(0)==0
+def test_str():
+    with pytest.raises(TypeError):
+        square("cat")
