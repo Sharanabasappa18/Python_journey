65618f0a1e2c30c38a571af3ddac1246eda82f12
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-09-13 16:14:57 +0530

    comment

--- calculator_2.py
+++ calculator_2.py
@@ -2,6 +2,6 @@ def main():
     x=int(input("What's x ? "))
     print("x square is", square(x))
 def square(n):
-    return n*n
+    return n*n              # for testing calculator use "+"sign 
 if __name__=="__main__":
     main()
