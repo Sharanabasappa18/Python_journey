0a1e1d55344b6af007fc4a7944be5c7c47fbf8b6
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-09-14 01:48:48 +0530

    Add pytest tests for hello function

---
+++ test_hello.py
@@ -0,0 +1,11 @@
+from hello import hello
+
+def test_default():
+    assert hello()=="hello, World"
+    
+def test_argument():
+    for name in ["Hermione","Harry","Ron"]:
+        assert hello(name)==f"hello, {name}"
+
+def test_hello():
+    assert hello("David")=="hello, David"
