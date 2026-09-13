e9f0b54c9b5b20c37db537580cbfeed72bbed3b9
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-09-14 01:46:15 +0530

    Add hello function with user input

--- hello.py
+++ hello.py
@@ -1,6 +1,16 @@
-name = input("what's your name? ")
-# print("hello,",name,sep="bsdk")
-print("hello, ",end="")
-name=name.strip()
-name=name.title()
-print(name)
+# name = input("what's your name? ")
+# # print("hello,",name,sep="bsdk")
+# print("hello, ",end="")
+# name=name.strip()
+# name=name.title()
+# print(name)
+
+def main():
+    name=input("What s ur name?")
+    print(hello(name))
+def hello(to="World"):
+    return f"hello, {to}"
+    
+if __name__=="__main__":
+    main()
+    
