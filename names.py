d43bdb9708763bc86f27f3de92edfd32b3253026
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-09-15 22:34:42 +0530

    Refactor file writing to use context manager (with open)

--- names.py
+++ names.py
@@ -8,6 +8,8 @@
 
 name=input("What's your name? ")
 
-file =open("names.txt","a")
-file.write(f"{name}\n")
-file.close()
+# file =open("names.txt","a")
+with open("names.txt","a") as file:
+    file.write(f"{name}\n")
+
+# file.close()
