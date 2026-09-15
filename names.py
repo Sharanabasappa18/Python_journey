c2f2cecbeea64c392ee01260124f4ec1730048cd
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-09-15 23:17:33 +0530

    Read and print sorted names from names.txt

--- names.py
+++ names.py
@@ -13,3 +13,15 @@ with open("names.txt","a") as file:
     file.write(f"{name}\n")
 
 # file.close()
+
+with open("names.txt","r") as file:
+    for line in file:
+        print("hello,",line.rstrip()) 
+        
+### for printing names in sorted way ###
+names=[]
+with open("names.txt") as file:
+    for line in file:
+        names.append(line.rstrip())
+for name in sorted(names):
+    print(f"hello,{name}")
