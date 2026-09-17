20b080caf0ce271183adfecdd6b04c735fb4efab
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-09-17 22:06:04 +0530

    Sort students using lambda function

--- students.py
+++ students.py
@@ -7,11 +7,43 @@
         
         
 ### printing in sorted way ###
+# students=[]
+# with open("students.csv") as file:
+#     for line in file:
+#         name,house=line.rstrip().split(",")
+#         students.append(f"{name} is in {house}")
+        
+# for student in sorted(students):
+#     print(student)
+    
+
+#### By defining a function ####
 students=[]
 with open("students.csv") as file:
     for line in file:
         name,house=line.rstrip().split(",")
-        students.append(f"{name} is in {house}")
+        student={"name":name,"house":house}
+        students.append(student)
         
-for student in sorted(students):
-    print(student)
+# def get_name(student):         
+    # return student["name"]
+
+# for student in sorted(students,key=get_name,reverse=True):    # use only when defining a function 
+for student in sorted(students,key=lambda student:student["name"]):     # use lambda instead of function 
+    print(f"{student['name']} is in {student['house']}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
