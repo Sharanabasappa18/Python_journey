75615baf01c8f5e74bec4f634697fca1eed84447
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-09-17 18:17:55 +0530

    Read and sort student data from CSV

--- students.py
+++ students.py
@@ -1,7 +1,17 @@
+# with open("students.csv") as file:
+#     for line in file:
+#         # row=line.rstrip().split(",")
+#         name,house=line.rstrip().split(",")
+#         # print(f"{row[0]} is in {row[1]}")
+#         print(f"{name} is in {house}")
+        
+        
+### printing in sorted way ###
+students=[]
 with open("students.csv") as file:
     for line in file:
-        # row=line.rstrip().split(",")
         name,house=line.rstrip().split(",")
-        # print(f"{row[0]} is in {row[1]}")
-        print(f"{name} is in {house}")
+        students.append(f"{name} is in {house}")
         
+for student in sorted(students):
+    print(student)
