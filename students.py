e3201ca022adcacaa29bafd6cb8b0a1886fe0bfe
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-09-17 23:34:14 +0530

    Add student data to CSV using csv.writer

--- students.py
+++ students.py
@@ -18,23 +18,47 @@
     
 
 #### By defining a function ####
-students=[]
-with open("students.csv") as file:
-    for line in file:
-        name,house=line.rstrip().split(",")
-        student={"name":name,"house":house}
-        students.append(student)
+# students=[]
+# with open("students.csv") as file:
+#     for line in file:
+#         name,house=line.rstrip().split(",")
+#         student={"name":name,"house":house}
+#         students.append(student)
         
-# def get_name(student):         
-    # return student["name"]
+# # def get_name(student):         
+#     # return student["name"]
 
-# for student in sorted(students,key=get_name,reverse=True):    # use only when defining a function 
-for student in sorted(students,key=lambda student:student["name"]):     # use lambda instead of function 
-    print(f"{student['name']} is in {student['house']}")
-
-
+# # for student in sorted(students,key=get_name,reverse=True):    # use only when defining a function 
+# for student in sorted(students,key=lambda student:student["name"]):     # use lambda instead of function 
+#     print(f"{student['name']} is in {student['house']}")
 
 
+
+## using csv.reader function ##
+# import csv
+# students=[]
+# with open("students.csv") as file:
+#     # reader=csv.reader(file)
+#     # for name,home in reader:
+#         # students.append({"name":name,"home":home})
+#     reader=csv.DictReader(file)
+#     for row in reader:
+#         students.append({"name":row["name"],"home":row["home"]})
+
+ 
+# for student in sorted(students,key=lambda student:student["name"]):
+#     print(f"{student['name']} is from {student['home']}")
+        
+
+############ for writing the csv file #############
+import csv
+
+name = input("What's your name? ")
+home = input("Where is your home? ")
+
+with open("students.csv", "a", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow([name, home])
 
 
 
