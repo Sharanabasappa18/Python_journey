92df77a3a95fa889d95914427efecd47d7878e1f
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-09-22 21:24:14 +0530

    add finding even & odd  numbers in a list

--- lists.py
+++ lists.py
@@ -1,5 +1,17 @@
-# finding 2nd largest number in list 
+###finding 2nd largest number in list ###
 numbers=[10,45,23,67,89,69]
-unique_numbers=list(set(numbers))
-unique_numbers.sort()
-print("2nd largest number:",unique_numbers[-2])
+# unique_numbers=list(set(numbers))
+# unique_numbers.sort()
+# print("2nd largest number:",unique_numbers[-2])
+
+###finding even & odd numbers in list ####
+even=[]
+odd=[]
+
+for num in numbers:
+    if num%2==0:
+        even.append(num)
+    else:
+        odd.append(num)
+print("Even:",even)
+print("Odd:",odd)
