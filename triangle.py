c49f4a364aace14cd9767664d545ba5864009348
Sharanabasappa18 <sharanabasappadubale365@gmail.com>
2026-09-25 01:37:03 +0530

    add comment to make it reverse logic

--- triangle.py
+++ triangle.py
@@ -1,6 +1,6 @@
 for i in range(1,6):
     for j in range(1,6):
-        if j<=i:
+        if j<=i:                 ## for reverse "j>=i" ##
             print("*",end="")
         else:
             print("",end="")
